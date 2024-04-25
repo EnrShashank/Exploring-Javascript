888110ba3aa44dab864c77d6962c4620277b9b6c
Shashank Singh <148775528+EnrShashank@users.noreply.github.com>
2024-04-25 15:55:57 +0000

    Basics of Array

---
+++ 01_jsBasic/array.js
@@ -0,0 +1,10 @@
+// arrays
+
+const arr = [1,2,3,4,"Shashank",true]
+console.log(arr);
+
+//arr.unshift(11); // put the value to the first hence this is not the optimized way.
+arr.shift(3);
+
+console.log(arr.includes("Shashank"));
+console.log(arr);
