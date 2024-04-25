03818d885e21b8f484c746818302a99b7928a301
Shashank Singh <148775528+EnrShashank@users.noreply.github.com>
2024-04-25 16:03:31 +0000

    methods used in array

--- 01_jsBasic/array.js
+++ 01_jsBasic/array.js
@@ -1,10 +1,18 @@
 // arrays
 
-const arr = [1,2,3,4,"Shashank",true]
-console.log(arr);
+// const arr = [1,2,3,4,"Shashank",true]
+// console.log(arr);
 
 //arr.unshift(11); // put the value to the first hence this is not the optimized way.
-arr.shift(3);
+// arr.shift(3);
 
-console.log(arr.includes("Shashank"));
-console.log(arr);
+// console.log(arr.includes("Shashank"));
+// console.log(arr);
+
+const myarr = [0,1,2,3,4,5,6,7,8];
+console.log(myarr);
+
+console.log(myarr.splice(1,4)); //splice() changes the contents of the original array.
+console.log("b",myarr);
+
+console.log(myarr.slice(1,4)); //slice() extracts elements into a new array without modifying the original.
